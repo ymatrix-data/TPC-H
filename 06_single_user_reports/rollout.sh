03eece27822b5f1674b031ff22fca4eb26c80cb7
caowei <weicao92@163.com>
2021-03-05 08:55:46 +0800

    refactor for sql and single user mode config to be effective

--- 06_single_user_reports/rollout.sh
+++ 06_single_user_reports/rollout.sh
@@ -1,61 +1,67 @@
 #!/bin/bash
 set -e
 
-GEN_DATA_DIR=${11}
+RUN_SINGLE_USER_REPORT=${14}
 
-PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source $PWD/../functions.sh
-source_bashrc
-step=single_user_reports
+if [ "$RUN_SINGLE_USER_REPORT" == "true" ]; then
+  GEN_DATA_DIR=${11}
 
-init_log $step
+  PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+  source $PWD/../functions.sh
+  source_bashrc
+  step=single_user_reports
 
-get_version
-if [[ "$VERSION" == *"gpdb"* ]]; then
-	filter="gpdb"
-elif [ "$VERSION" == "postgresql" ]; then
-	filter="postgresql"
+  init_log $step
+
+  get_version
+  if [[ "$VERSION" == *"gpdb"* ]]; then
+    filter="gpdb"
+  elif [ "$VERSION" == "postgresql" ]; then
+    filter="postgresql"
+  else
+    echo "ERROR: Unsupported VERSION!"
+    exit 1
+  fi
+
+  for i in $(ls $PWD/*.$filter.*.sql); do
+    echo "psql -v ON_ERROR_STOP=1 -a -f $i"
+    psql -v ON_ERROR_STOP=1 -a -f $i
+    echo ""
+  done
+
+  for i in $(ls $PWD/*.copy.*.sql); do
+    logstep=$(echo $i | awk -F 'copy.' '{print $2}' | awk -F '.' '{print $1}')
+    logfile="$GEN_DATA_DIR/log/rollout_""$logstep"".log"
+    logfile="'""$logfile""'"
+    echo "psql -v ON_ERROR_STOP=1 -a -f $i -v LOGFILE=\"$logfile\""
+    psql -v ON_ERROR_STOP=1 -a -f $i -v LOGFILE="$logfile"
+    echo ""
+  done
+
+  psql -v ON_ERROR_STOP=1 -q -t -A -c "select 'analyze ' || n.nspname || '.' || c.relname || ';' from pg_class c join pg_namespace n on n.oid = c.relnamespace and n.nspname = 'tpch_reports'" | psql -v ON_ERROR_STOP=1 -t -A -e
+
+  echo "********************************************************************************"
+  echo "Generate Data"
+  echo "********************************************************************************"
+  psql -F $'\t' -A -v ON_ERROR_STOP=1 -P pager=off -f $PWD/gen_data_report.sql
+  echo ""
+  echo "********************************************************************************"
+  echo "Data Loads"
+  echo "********************************************************************************"
+  psql -F $'\t' -A -v ON_ERROR_STOP=1 -P pager=off -f $PWD/loads_report.sql
+  echo ""
+  echo "********************************************************************************"
+  echo "Analyze"
+  echo "********************************************************************************"
+  psql -F $'\t' -A -v ON_ERROR_STOP=1 -P pager=off -f $PWD/analyze_report.sql
+  echo ""
+  echo ""
+  echo "********************************************************************************"
+  echo "Queries"
+  echo "********************************************************************************"
+  psql -F $'\t' -A -v ON_ERROR_STOP=1 -P pager=off -f $PWD/queries_report.sql
+  echo ""
+  end_step $step
 else
-	echo "ERROR: Unsupported VERSION!"
-	exit 1
+  echo "skipping RUN_SINGLE_USER_REPORT step ..."
 fi
-
-for i in $(ls $PWD/*.$filter.*.sql); do
-	echo "psql -v ON_ERROR_STOP=1 -a -f $i"
-	psql -v ON_ERROR_STOP=1 -a -f $i
-	echo ""
-done
-
-for i in $(ls $PWD/*.copy.*.sql); do
-	logstep=$(echo $i | awk -F 'copy.' '{print $2}' | awk -F '.' '{print $1}')
-	logfile="$GEN_DATA_DIR/log/rollout_""$logstep"".log"
-	logfile="'""$logfile""'"
-	echo "psql -v ON_ERROR_STOP=1 -a -f $i -v LOGFILE=\"$logfile\""
-	psql -v ON_ERROR_STOP=1 -a -f $i -v LOGFILE="$logfile"
-	echo ""
-done
-
-psql -v ON_ERROR_STOP=1 -q -t -A -c "select 'analyze ' || n.nspname || '.' || c.relname || ';' from pg_class c join pg_namespace n on n.oid = c.relnamespace and n.nspname = 'tpch_reports'" | psql -v ON_ERROR_STOP=1 -t -A -e
-
-echo "********************************************************************************"
-echo "Generate Data"
-echo "********************************************************************************"
-psql -F $'\t' -A -v ON_ERROR_STOP=1 -P pager=off -f $PWD/gen_data_report.sql
-echo ""
-echo "********************************************************************************"
-echo "Data Loads"
-echo "********************************************************************************"
-psql -F $'\t' -A -v ON_ERROR_STOP=1 -P pager=off -f $PWD/loads_report.sql
-echo ""
-echo "********************************************************************************"
-echo "Analyze"
-echo "********************************************************************************"
-psql -F $'\t' -A -v ON_ERROR_STOP=1 -P pager=off -f $PWD/analyze_report.sql
-echo ""
-echo ""
-echo "********************************************************************************"
-echo "Queries"
-echo "********************************************************************************"
-psql -F $'\t' -A -v ON_ERROR_STOP=1 -P pager=off -f $PWD/queries_report.sql
-echo ""
-end_step $step
